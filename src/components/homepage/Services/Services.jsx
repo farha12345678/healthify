4821b030a3cae27e08f6afb310e7e91c6996e515
farha12345678 <sabikunnaharfarha01@gmail.com>
2024-11-30 21:54:12 +0600

    service section modified

--- src/components/homepage/Services/Services.jsx
+++ src/components/homepage/Services/Services.jsx
@@ -31,21 +31,29 @@ const Services = () => {
                         <h1 className="text-5xl font-bold">We provide...</h1>
                         {/* points */}
                        <div>
-                        <div>
+                        <div className="flex gap-x-4  text-xl text-gray-500 font-medium my-5">
                             <p><FaCheckCircle /></p>
                             <p>Personalized routine</p>
                         </div>
-                        <div>
+                        <div className="flex gap-x-4 text-xl text-gray-500 font-medium mb-5">
                             <p><FaCheckCircle /></p>
                             <p>Follow-up after completing courses</p>
                         </div>
-                        <div>
-                            <p></p>
-                            <p></p>
+                        <div className="border mb-5"></div>
+                        <div className="mb-5">
+                        <div className="flex gap-x-4 font-medium ">
+                            <p className="text-purple-400 text-xl"><FaCheckCircle /></p>
+                            <p className="text-black text-xl">Access to additional resources</p>
                         </div>
-                        <div>
-                            <p></p>
-                            <p></p>
+                        <p className="ml-8">We offer access to a variety of additional resources to <br /> enhance your experience, including exclusive tools, guides <br /> and support materials designed to help you achieve your <br /> goals more effectively.</p>
+                        </div>
+                        <div className="border mb-5"></div>
+                        <div className="flex gap-x-4 text-xl text-gray-500 font-medium">
+                            <p className=""><FaCheckCircle /></p>
+                            <p >Free community support</p>
+                        </div>
+                        <div className="mt-10">
+                            <button className="text-[#287279] border border-[#287279] p-2">Learn More About Our Services</button>
                         </div>
                        </div>
                        
