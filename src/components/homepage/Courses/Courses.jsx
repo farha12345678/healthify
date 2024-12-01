21abdec63aa201578f886e9a787c4e7e36e57133
farha12345678 <sabikunnaharfarha01@gmail.com>
2024-12-01 21:48:41 +0600

    border added

--- src/components/homepage/Courses/Courses.jsx
+++ src/components/homepage/Courses/Courses.jsx
@@ -81,7 +81,7 @@ const Courses = () => {
             ref={swiperRef} 
           >
           <SwiperSlide>
-  <div className="card card-compact bg-base-100 w-80 relative group border border-transparent hover:border-purple-400 hover:bg-purple-100 transition-transform duration-300 ease-in-out">
+  <div className="card card-compact bg-base-100 w-80 relative group border  hover:border-purple-400 hover:bg-purple-100 transition-transform duration-300 ease-in-out">
     
     <figure className="h-48 relative">
       <Image
@@ -174,7 +174,7 @@ const Courses = () => {
   </div>
 </SwiperSlide>
 <SwiperSlide>
-              <div className="card card-compact bg-base-100 w-80 relative group border border-transparent hover:border-purple-400 hover:bg-purple-100 transition-transform duration-300 ease-in-out">
+              <div className="card card-compact bg-base-100 w-80 relative group border hover:border-purple-400 hover:bg-purple-100 transition-transform duration-300 ease-in-out">
                 <figure className='h-48'>
                   <Image
                     src={img2}
@@ -241,7 +241,7 @@ const Courses = () => {
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className="card card-compact bg-base-100 w-80 relative group border border-transparent hover:border-purple-400 hover:bg-purple-100 transition-transform duration-300 ease-in-out">
+              <div className="card card-compact bg-base-100 w-80 relative group border hover:border-purple-400 hover:bg-purple-100 transition-transform duration-300 ease-in-out">
                 <figure className='h-48'>
                   <Image
                     src={img3}
@@ -312,7 +312,7 @@ const Courses = () => {
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className="card card-compact bg-base-100 w-80 relative group border border-transparent hover:border-purple-400 hover:bg-purple-100 transition-transform duration-300 ease-in-out">
+              <div className="card card-compact bg-base-100 w-80 relative group border hover:border-purple-400 hover:bg-purple-100 transition-transform duration-300 ease-in-out">
                 <figure className='h-48'>
                   <Image
                     src={img4}
@@ -383,242 +383,7 @@ const Courses = () => {
         </div>
       </div>
       <div>
-        {/* Card */}
-        <div className='flex gap-x-4'>
-          {/* <div className="card card-compact bg-base-100 w-80 shadow-xl">
-                        <figure>
-                            <Image
-                                src={img1}
-                                alt="Shoes" 
-                                className='h-64 object-cover'/>
-                        </figure>
-                        <div className='flex gap-x-2 mt-3 ml-4 text-xs'>
-                            <p className='p-1 bg-red-50 border border-red-200 rounded-lg'>Beginner</p>
-                            <p className='p-1 bg-yellow-50 border border-yellow-200 rounded-lg'>Nutrition and Diet</p>
-                        </div>
-                        <div className="card-body">
-
-                            <h2 className="card-title">Foundation of Sleep: Sleep Science and sleep disorders</h2>
-                           
-                           <div className='flex gap-x-4'>
-                           <div className="rating">
-                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                                <input
-                                    type="radio"
-                                    name="rating-2"
-                                    className="mask mask-star-2 bg-orange-400"
-                                    />
-                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" defaultChecked  />
-                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                            </div>
-                            <p>4.9(566)</p>
-                           </div>
-                          
-                           <div className='flex gap-x-4 mt-10'>
-                            <div className='flex gap-x-1'>
-                                <p className='text-xl text-purple-400'><MdWatchLater /></p>
-                                <p>6h 34min</p>
-                            </div>
-                            <div className="h-4 border-l-2 border-gray-300"></div>
-                            <div className='flex gap-x-1'>
-                                <p className='text-xl text-purple-400'><IoBookSharp /></p>
-                                <p>3 Lessons</p>
-                            </div>
-                           </div>
-                           <div className='border'></div>
-                           <div className='flex justify-between mb-2'>
-                           <div className='flex gap-x-2'>
-                                <Image
-                                src={auth1}
-                                alt='author'
-                                className='w-8 h-8 rounded-full'
-                                />
-                                <p className='font-bold mt-2'>Killian Murphe</p>
-                            </div>
-                            <div>
-                                <p className='font-bold text-xl'>$40</p>
-                            </div>
-                           </div>
-                        </div>
-                    </div> */}
-          {/* <div className="card card-compact bg-base-100 w-80 shadow-xl">
-                        <figure>
-                            <Image
-                                src={img2}
-                                alt="Shoes"
-                                className='h-64 object-cover' />
-                        </figure>
-                        <div className='flex gap-x-2 mt-3 ml-4 text-xs'>
-                            <p className='p-1 bg-red-50 border border-red-200 rounded-lg'>Intermediate</p>
-                            <p className='p-1 bg-yellow-50 border border-yellow-200 rounded-lg'>Health and Wellness</p>
-                        </div>
-                        <div className="card-body">
-
-                            <h2 className="card-title">Parenting in the Digital Age: Navigating screen time</h2>
-                           
-                           <div className='flex gap-x-4'>
-                           <div className="rating">
-                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                                <input
-                                    type="radio"
-                                    name="rating-2"
-                                    className="mask mask-star-2 bg-orange-400"
-                                    />
-                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" defaultChecked  />
-                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                            </div>
-                            <p>4.9(566)</p>
-                           </div>
-                          
-                           <div className='flex gap-x-4 mt-10'>
-                            <div className='flex gap-x-1'>
-                                <p className='text-xl text-purple-400'><MdWatchLater /></p>
-                                <p>6h 34min</p>
-                            </div>
-                            <div className="h-4 border-l-2 border-gray-300"></div>
-                            <div className='flex gap-x-1'>
-                                <p className='text-xl text-purple-400'><IoBookSharp /></p>
-                                <p>3 Lessons</p>
-                            </div>
-                           </div>
-                           <div className='border'></div>
-                           <div className='flex justify-between mb-2'>
-                           <div className='flex gap-x-2'>
-                                <Image
-                                src={auth2}
-                                alt='author'
-                                className='w-8 h-8 rounded-full'
-                                />
-                                <p className='font-bold mt-2'>Sarah Hopkins</p>
-                            </div>
-                            <div>
-                                <p className='font-bold text-xl'>$50</p>
-                            </div>
-                           </div>
-                        </div>
-                    </div> */}
-          {/* <div className="card card-compact bg-base-100 w-80 shadow-xl">
-                        <figure>
-                            <Image
-                                src={img3}
-                                alt="Shoes"
-                                className='h-64 object-cover' />
-                                
-                        </figure>
-                        <div className='flex gap-x-2 mt-3 ml-4 text-xs'>
-                            <p className='p-1 bg-red-50 border border-red-200 rounded-lg'>Beginner</p>
-                            <p className='p-1 bg-yellow-50 border border-yellow-200 rounded-lg'>Nutrition and Diet</p>
-                        </div>
-                        <div className="card-body">
-
-                            <h2 className="card-title">Holistic Health: Integrating Mind, Body and Spirit</h2>
-                            
-                           <div className='flex gap-x-4'>
-                           <div className="rating">
-                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                                <input
-                                    type="radio"
-                                    name="rating-2"
-                                    className="mask mask-star-2 bg-orange-400"
-                                    />
-                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" defaultChecked  />
-                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                            </div>
-                            <p>4.9(566)</p>
-                           </div>
-                          
-                           <div className='flex gap-x-4 mt-10'>
-                            <div className='flex gap-x-1'>
-                                <p className='text-xl text-purple-400'><MdWatchLater /></p>
-                                <p>6h 34min</p>
-                            </div>
-                            <div className="h-4 border-l-2 border-gray-300"></div>
-                            <div className='flex gap-x-1'>
-                                <p className='text-xl text-purple-400'><IoBookSharp /></p>
-                                <p>3 Lessons</p>
-                            </div>
-                           </div>
-                           <div className='border'></div>
-                           <div className='flex justify-between mb-2'>
-                           <div className='flex gap-x-2'>
-                                <Image
-                                src={auth3}
-                                alt='author'
-                                className='w-8 h-8 rounded-full'
-                                />
-                                <p className='font-bold mt-2'>Luna Karim</p>
-                            </div>
-                            <div>
-                               <div className='flex gap-x-1 p-1 bg-gray-100 border border-gray-400 text-gray-600 rounded-lg'>
-                                <p className='text-base text-gray-500'><FaCheckCircle /></p>
-                                <p className='text-xs'>Enrolled</p>
-                               </div>
-                            </div>
-                           </div>
-                        </div>
-                    </div> */}
-          {/* <div className="card card-compact bg-base-100 w-80 shadow-xl">
-                        <figure>
-                            <Image
-                                src={img4}
-                                alt="Shoes"
-                                className='h-64 object-cover'
-                                 />
-                        </figure>
-                        <div className='flex gap-x-2 mt-3 ml-4 text-xs'>
-                            <p className='p-1 bg-red-50 border border-red-200 rounded-lg'>Beginner</p>
-                            <p className='p-1 bg-yellow-50 border border-yellow-200 rounded-lg'>Nutrition and Diet</p>
-                        </div>
-                        <div className="card-body">
-
-                            <h2 className="card-title">Introduction to Health and Nutrition</h2>
-                           
-                           <div className='flex gap-x-4'>
-                           <div className="rating">
-                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                                <input
-                                    type="radio"
-                                    name="rating-2"
-                                    className="mask mask-star-2 bg-orange-400"
-                                    />
-                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" defaultChecked  />
-                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                            </div>
-                            <p>4.9(566)</p>
-                           </div>
-                          
-                           <div className='flex gap-x-4 mt-10'>
-                            <div className='flex gap-x-1'>
-                                <p className='text-xl text-purple-400'><MdWatchLater /></p>
-                                <p>6h 34min</p>
-                            </div>
-                            <div className="h-4 border-l-2 border-gray-300"></div>
-                            <div className='flex gap-x-1'>
-                                <p className='text-xl text-purple-400'><IoBookSharp /></p>
-                                <p>3 Lessons</p>
-                            </div>
-                           </div>
-                           <div className='border'></div>
-                           <div className='flex justify-between mb-2'>
-                           <div className='flex gap-x-2'>
-                                <Image
-                                src={auth4}
-                                alt='author'
-                                className='w-8 h-8 rounded-full'
-                                />
-                                <p className='font-bold mt-2'>Kate Winslate</p>
-                            </div>
-                            <div>
-                                <p className='font-bold text-xl'>$40</p>
-                            </div>
-                           </div>
-                        </div>
-                    </div> */}
-        </div>
+       
       </div>
     </div>
   );
